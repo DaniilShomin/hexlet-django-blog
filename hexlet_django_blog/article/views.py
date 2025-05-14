681cd8a9cba8097518cd5c61fd742ea983b3e54a
DaniilShomin <d.sh.v@yandex.ru>
2025-05-14 20:07:29 +0300

    add a main template for the application

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("article")
+    title = 'Django project'
+    return render(
+        request,
+        'articles/index.html',
+        context={
+            'title': title,
+        },
+    )
